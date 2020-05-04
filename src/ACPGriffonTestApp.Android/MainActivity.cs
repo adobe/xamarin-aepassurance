b08ec178162e5057ad2bf3871dfbb52ff4b7b8c8
Ryan Morales <rymorale@adobe.com>
2020-05-04 09:45:11 -0700

    use latest 1.5.3 android core release
    - also remove unneeded init line in android app
    - update androidmanifest for griffon

--- src/ACPGriffonTestApp.Android/MainActivity.cs
+++ src/ACPGriffonTestApp.Android/MainActivity.cs
@@ -34,7 +34,6 @@ namespace ACPGriffonTestApp.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
             // set log level
