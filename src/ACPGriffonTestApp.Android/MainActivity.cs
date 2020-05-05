aab82b2f81349ef2fac2553fa1e9466b9a7d009b
Ryan Morales <rymorale@adobe.com>
2020-05-05 11:49:39 -0700

    use app created under obumobile5 org

--- src/ACPGriffonTestApp.Android/MainActivity.cs
+++ src/ACPGriffonTestApp.Android/MainActivity.cs
@@ -16,7 +16,7 @@ namespace ACPGriffonTestApp.Droid
         public void Call(Java.Lang.Object callback)
         {
             // set launch config
-            ACPCore.ConfigureWithAppID("launch-ENf8ed5382efc84d5b81a9be8dcc231be1-development");
+            ACPCore.ConfigureWithAppID("94f571f308d5/00fc543a60e1/launch-c861fab912f7-development");
         }
     }
 
